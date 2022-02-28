b1296f4cf4082b0d79d31609558c4a2f734cdc88
mahesh8492 <97870847+mahesh8492@users.noreply.github.com>
2022-02-28 16:49:13 +0530

    Add files via upload

---
+++ 85-OLX-Advertises/src/main/java/com/zensar/olx/bean/AdvertismentStatus.java
@@ -0,0 +1,68 @@
+package com.zensar.olx.bean;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Embeddable
+public class AdvertismentStatus {
+	@Column(name="status_id")
+	private int id;
+	@Transient
+	private String status;
+	public AdvertismentStatus(int id, String status) {
+		super();
+		this.id = id;
+		this.status = status;
+	}
+	public AdvertismentStatus() {
+		super();
+	}
+	public AdvertismentStatus(String status) {
+		super();
+		this.status = status;
+	}
+	public AdvertismentStatus(int id) {
+		super();
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "AdvertismentStatus [id=" + id + ", status=" + status + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AdvertismentStatus other = (AdvertismentStatus) obj;
+		return id == other.id;
+	}
+	
+}
